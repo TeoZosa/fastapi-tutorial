c64dd3f238dc439e0bd4ca3a227df79bf54be137
Teo Zosa <teofilo@sonosim.com>
2020-09-30 17:45:29 -0700

    Add "Path Operation Configuration" section

--- fastAPI_tutorial/main.py
+++ fastAPI_tutorial/main.py
@@ -1490,3 +1490,97 @@ async def read_item(item_id: int):
     if item_id == 3:
         raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
     return {"item_id": item_id}
+
+
+"""Path Operation Configuration"""
+
+## Tags
+## Deprecate a path operation
+from typing import Optional, Set
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: Set[str] = []
+
+
+@app.post("/items/", 
+          response_model=Item,
+
+          # Add tags to your path operation, 
+          # pass the parameter tags with a 
+          # list of str (commonly just one str):
+          tags=["items"])
+async def create_item(item: Item):
+    return item
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
+
+
+@app.get("/elements/", tags=["items"], 
+         # Mark a path operation as deprecated, but without removing it
+         deprecated=True)
+async def read_elements():
+    return [{"item_id": "Foo"}]
+
+
+## Summary and Description
+## Description from docstring
+## Response description
+from typing import Optional, Set
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: Set[str] = []
+
+
+@app.post("/items/", 
+          response_model=Item, 
+          summary="Create an item",
+          
+          # Specify PATH OPERATION description 
+          # with plaintext
+          #description="Create an item with all the information:",
+
+          # Specify the RESPONSE description
+          response_description="The created item",
+          )
+
+# Specify the PATH OPERATION description 
+# with rich text (Markdown) via docstring 
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+    return item
