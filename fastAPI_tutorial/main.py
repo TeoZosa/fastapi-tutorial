c83331760f23b1abd544d6b02aa461cc1cfe7b51
Teo Zosa <teofilo@sonosim.com>
2020-09-30 18:32:59 -0700

    Add "Body - Updates" section

--- fastAPI_tutorial/main.py
+++ fastAPI_tutorial/main.py
@@ -1629,3 +1629,119 @@ def update_item(id: str, item: Item):
 # It doesn't return a large str containing the data in JSON format (as a string). 
 # It returns a Python standard data structure (e.g. a dict) 
 # with values and sub-values that are all compatible with JSON.
+
+"""Body - Updates"""
+# Update replacing with PUT
+from typing import List, Optional
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items = {
+        "foo": {"name": "Foo", "price": 50.2},
+        "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+        "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+        }
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    # REPLACEMENT
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+# WARNING about replacing¶
+# That means that if you want to update the item bar using PUT with a body containing:
+# {
+#         "name": "Barz",
+#         "price": 3,
+#         "description": None,
+#         }
+# because it doesn't include the already stored attribute "tax": 20.2, 
+# the input model would take the default value of "tax": 10.5.
+# And the data would be saved with that "new" tax of 10.5.
+
+## Partial updates with Patch
+from typing import List, Optional
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items = {
+        "foo": {"name": "Foo", "price": 50.2},
+        "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+        "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+        }
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+@app.patch("/items/{item_id}", response_model=Item)
+# Note:
+# PATCH is less commonly used and known than PUT.
+    # And many teams use only PUT, even for partial updates.
+    # You are free to use them however you want, FastAPI doesn't impose any restrictions.
+# But this guide shows you, more or less, how they are INTENDED to be used.
+    # You can actually use this same technique with an HTTP PUT operation.
+    # But the example here uses PATCH because it was created for these use cases.
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    
+    # Using Pydantic's exclude_unset parameter
+    update_data = item.dict(exclude_unset=True)
+    # If you want to receive partial updates, 
+    # it's very useful to use the parameter 
+    # `exclude_unset` in Pydantic's model's `.dict()`
+    # That would generate a dict with 
+        # only the data that was set when creating the item model, excluding default values.
+    # Then you can use this to generate a dict 
+    # with only the data that was set (sent in the request), 
+    # omitting default values
+    
+    #Using Pydantic's update parameter
+    updated_item = stored_item_model.copy(update=update_data)
+    # Now, you can create a copy of the existing model using `.copy()`, 
+    # and pass the update parameter with a dict containing the data to update.
+    
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
+# Notice that the input model is still validated.
+
+# So, if you want to receive partial updates that can omit all the attributes, 
+# you need to have a model with all the attributes marked as optional 
+# (with default values or None).
+
+# To distinguish from the models with all optional values for updates 
+# and models with required values for creation, 
+# you can use the ideas described in "Extra Models".
