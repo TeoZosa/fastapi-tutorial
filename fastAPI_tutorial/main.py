2b3e79d26116181e2e5d167ea0525ebf15b9582a
Teo Zosa <teofilo@sonosim.com>
2020-09-30 18:18:53 -0700

    Add "JSON Compatible Encoder" section

--- fastAPI_tutorial/main.py
+++ fastAPI_tutorial/main.py
@@ -1584,3 +1584,48 @@ async def create_item(item: Item):
     - **tags**: a set of unique tag strings for this item
     """
     return item
+
+"""JSON Compatible Encoder"""
+from datetime import datetime
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+# Let's imagine that you have a database 
+# that only receives JSON compatible data.
+fake_db = {}
+# For example, 
+    # it doesn't receive datetime objects, as those are not compatible with JSON.
+    # So, a datetime object 
+    # would have to be converted to a str containing the data in ISO format.
+
+    # The same way, 
+    # this database wouldn't receive a Pydantic model (an object with attributes), 
+    # only a dict.
+
+# You can use jsonable_encoder for that.
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: Optional[str] = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    # It receives an object, like a Pydantic model, 
+    # and returns a JSON compatible version:
+    json_compatible_item_data = jsonable_encoder(item)
+    # In this example, it would 
+        # convert the Pydantic model to a dict, 
+        # and the datetime to a str.
+    fake_db[id] = json_compatible_item_data
+# Note: 
+# It doesn't return a large str containing the data in JSON format (as a string). 
+# It returns a Python standard data structure (e.g. a dict) 
+# with values and sub-values that are all compatible with JSON.
