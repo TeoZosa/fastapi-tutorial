5c00aa66b197cc4cda8bbfc1fda82ee6ef0ff794
Teo Zosa <teofilo@sonosim.com>
2020-10-01 09:31:27 -0700

    Add "Get Current User" section

--- fastAPI_tutorial/tutorial-user_guide/security.py
+++ fastAPI_tutorial/tutorial-user_guide/security.py
@@ -219,3 +219,54 @@ async def read_items(token: str =
 # Note:
 # We are not verifying the validity of the token yet, 
 # but that's a start already.
+
+"""Get Current User"""
+from typing import Optional
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+def fake_decode_token(token):
+    return User(
+            username=token + "fakedecoded",
+            email="john@example.com",
+            full_name="John Doe"
+            )
+
+
+async def get_current_user(
+        # Inject the token 
+        token: str = Depends(oauth2_scheme)):
+    # Create a Pydantic model (`User`) using the injected token
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(
+       
+        # Inject the current user
+        current_user: User = Depends(get_current_user)
+        # Note:
+        # Powerful <= this pattern can be used anywhere this dependency is required 
+        
+        ):
+    return current_user
+# Note: 
+    # The way this dependency system is designed allows us to have different 
+    # dependencies (different "dependables") that all return a User model.
+    # We are not restricted to having only one dependency that can return that 
+    # type of data.
